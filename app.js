eee37bca8d3d1d2f04fadfc054f1acb8aa0c5bcf
houssem-mbarki <89597665+houssem-mbarki@users.noreply.github.com>
2022-06-21 06:19:07 +0100

    Update app.js

--- app.js
+++ app.js
@@ -46,7 +46,7 @@ app.use("/api", brainTreeRouter);
 app.use("/api/order", orderRouter);
 app.use("/api/customize", customizeRouter);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || PORT;
 app.listen(PORT, () => {
   console.log("Server is running on ", PORT);
 });
